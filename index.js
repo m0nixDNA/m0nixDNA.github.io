8653791f4bc88dd91b0490c6f1f704972e553abc
m0nixDNA <145245361+m0nixDNA@users.noreply.github.com>
2023-09-18 00:25:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,11 +28,15 @@ const draw = () => {
 	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
-	context.fillStyle = '#999999';
+	context.fillStyle = '#696969';
 	context.font = fontSize + 'px monospace';
 
 	for(let i = 0; i < rainDrops.length; i++)
 	{
+		if (Math.random() > 0.0.995) {
+      context.fillStyle = '#999999';
+    }
+
 		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
 		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
 		
